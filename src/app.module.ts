ea1917fc070b8070346ef32acb6c2c16447f9397
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-11-17 23:20:07 +0530

    food orders

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReservationModule } from './restaurant/restaurant.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/sample_restaurants'),
+    ReservationModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
